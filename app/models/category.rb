b771988a52d32c7d6ccfd0f31e62e765ad63cfca
guillermo varela <guillovarela16@gmail.com>
2021-04-14 16:19:51 -0300

    Add some method to base controller and some validation to category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :user
+  has_many :products
+
+  validates :name, presence: true
+  validates :brand, presence: true
+  validates :quantity, presence: true
+  validates :price, presence: true
 end
