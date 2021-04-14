ae7d6c253cf792180d63c8d7b341bb179367eba0
guillermo varela <guillovarela16@gmail.com>
2021-04-14 16:22:16 -0300

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :categories, only: [ :index, :create, :update, :destroy ]
+      resources :products, only: [ :index, :create, :update, :destroy ]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
